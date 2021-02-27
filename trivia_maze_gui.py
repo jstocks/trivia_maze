f1e9f0af107433cacc7496706492dfe6c5cacd2b
jstocks <GitHubUW33>
2021-02-26 20:36:02 -0800

    2/26 8:30pm commits

--- trivia_maze_gui.py
+++ trivia_maze_gui.py
@@ -1,29 +1,11 @@
-import tkinter as tk
-# from tkinter.filedialog import askopenfilename, asksaveasfilename, Tk
-from PIL import ImageTk, Image
+from tkinter import *
+from PIL import ImageTk
 # from gameboard import GameBoard
 # from view import View, INTRO
 # from controller import Controller
-from tkinter import *
 
 
 class TriviaGUI(Frame):
-
-    # def menu_bar(self):
-    #     menu_bar = Menu(self)
-    #     file_menu = Menu(menu_bar, tearoff=0)
-    #     file_menu.add_command(label="New")
-    #     file_menu.add_command(label="Load")
-    #     file_menu.add_command(label="Save")
-    #     file_menu.add_command(label="Exit")
-    #     file_menu.add_cascade(label="File", menu=file_menu)
-    #
-    #     help_menu = Menu(self)
-    #     help_menu.add_command(label="Help")
-    #     help_menu.add_command(label="About")
-    #     help_menu.add_cascade(label="Help", menu=help_menu)
-    #
-    #     return menu_bar
 
     def menu_widgets(self):
         self.new = Button(self, text="NEW", height=1, width=5, bg="gold",
@@ -58,7 +40,7 @@ class TriviaGUI(Frame):
 
     def legend(self):
         label = Label(self.canvas, text='LEGEND', fg='black', bg='lightskyblue1', font="bold")
-        self.canvas.create_window(80, 500, window=label)
+        self.canvas.create_window(168, 490, window=label)
         # player token
         self.img = ImageTk.PhotoImage(file="player_token.png")
         self.canvas.create_image(80, 545, image=self.img)
@@ -71,11 +53,22 @@ class TriviaGUI(Frame):
         self.canvas.create_image(100, 600, image=self.img3)
         label2 = Label(self.canvas, text='Available Paths', fg='black', bg='lightskyblue1', font="bold")
         self.canvas.create_window(190, 600, window=label2)
-        # finish
-        self.img4 = ImageTk.PhotoImage(file="finish.png")
+        # blocked paths
+        self.img4 = ImageTk.PhotoImage(file="blocked.png")
         self.canvas.create_image(80, 660, image=self.img4)
-        label3 = Label(self.canvas, text='Finish', fg='black', bg='lightskyblue1', font="bold")
-        self.canvas.create_window(160, 660, window=label3)
+        label3 = Label(self.canvas, text='Blocked Paths', fg='black', bg='lightskyblue1', font="bold")
+        self.canvas.create_window(188, 660, window=label3)
+        # finish
+        self.img5 = ImageTk.PhotoImage(file="finish.png")
+        self.canvas.create_image(80, 720, image=self.img5)
+        label4 = Label(self.canvas, text='Finish', fg='black', bg='lightskyblue1', font="bold")
+        self.canvas.create_window(160, 720, window=label4)
+
+    def question(self):
+        label = Label(self.canvas, text='QUESTION', fg='black', bg='lightskyblue1', font="bold")
+        self.canvas.create_window(400, 490, window=label)
+
+        self.canvas.create_rectangle(360, 525, 750, 750, fill='lightskyblue1')
 
     def __init__(self, dimension, master=None):
         Frame.__init__(self, master)
@@ -90,6 +83,7 @@ class TriviaGUI(Frame):
         self.menu_widgets()
         self.move_widgets()
         self.legend()
+        self.question()
         # self.menu_bar()
 
     def show_board(self):
@@ -114,67 +108,67 @@ class TriviaGUI(Frame):
         y8 = y7 + size
 
         # Row 1
-        cell_1 = self.canvas.create_rectangle(x1, y1, x2, y2, fill='ivory2')
-        path_1_2 = self.canvas.create_rectangle(x2, y1, x3, y2, fill='red')
-        cell_2 = self.canvas.create_rectangle(x3, y1, x4, y2, fill='ivory2')
-        path_2_3 = self.canvas.create_rectangle(x4, y1, x5, y2, fill='red')
-        cell_3 = self.canvas.create_rectangle(x5, y1, x6, y2, fill='ivory2')
-        path_3_4 = self.canvas.create_rectangle(x6, y1, x7, y2, fill='red')
-        cell_4 = self.canvas.create_rectangle(x7, y1, x8, y2, fill='ivory2')
+        cell_1 = self.canvas.create_rectangle(x1, y1, x2, y2, fill='ivory2', outline="")
+        path_1_2 = self.canvas.create_rectangle(x2, y1, x3, y2, fill='red', outline="")
+        cell_2 = self.canvas.create_rectangle(x3, y1, x4, y2, fill='ivory2', outline="")
+        path_2_3 = self.canvas.create_rectangle(x4, y1, x5, y2, fill='red', outline="")
+        cell_3 = self.canvas.create_rectangle(x5, y1, x6, y2, fill='ivory2', outline="")
+        path_3_4 = self.canvas.create_rectangle(x6, y1, x7, y2, fill='red', outline="")
+        cell_4 = self.canvas.create_rectangle(x7, y1, x8, y2, fill='ivory2', outline="")
 
         # # Row 2
-        path_1_5 = self.canvas.create_rectangle(x1, y2, x2, y3, fill='red')
-        empty_1 = self.canvas.create_rectangle(x2, y2, x3, y3, fill='lightskyblue1')
-        path_2__6 = self.canvas.create_rectangle(x3, y2, x4, y3, fill='red')
-        empty_2 = self.canvas.create_rectangle(x4, y2, x5, y3, fill='lightskyblue1')
-        path_3_7 = self.canvas.create_rectangle(x5, y2, x6, y3, fill='red')
-        empty_3 = self.canvas.create_rectangle(x6, y2, x7, y3, fill='lightskyblue1')
-        path_4_8 = self.canvas.create_rectangle(x7, y2, x8, y3, fill='red')
+        path_1_5 = self.canvas.create_rectangle(x1, y2, x2, y3, fill='red', outline="")
+        empty_1 = self.canvas.create_rectangle(x2, y2, x3, y3, fill='lightskyblue1', outline="")
+        path_2__6 = self.canvas.create_rectangle(x3, y2, x4, y3, fill='red', outline="")
+        empty_2 = self.canvas.create_rectangle(x4, y2, x5, y3, fill='lightskyblue1', outline="")
+        path_3_7 = self.canvas.create_rectangle(x5, y2, x6, y3, fill='red', outline="")
+        empty_3 = self.canvas.create_rectangle(x6, y2, x7, y3, fill='lightskyblue1', outline="")
+        path_4_8 = self.canvas.create_rectangle(x7, y2, x8, y3, fill='red', outline="")
 
         # Row 3
-        cell_5 = self.canvas.create_rectangle(x1, y3, x2, y4, fill='ivory2')
-        path_5_6 = self.canvas.create_rectangle(x2, y3, x3, y4, fill='red')
-        cell_6 = self.canvas.create_rectangle(x3, y3, x4, y4, fill='ivory2')
-        path_6_7 = self.canvas.create_rectangle(x4, y3, x5, y4, fill='red')
-        cell_7 = self.canvas.create_rectangle(x5, y3, x6, y4, fill='ivory2')
-        path_7_8 = self.canvas.create_rectangle(x6, y3, x7, y4, fill='red')
-        cell_8 = self.canvas.create_rectangle(x7, y3, x8, y4, fill='ivory2')
+        cell_5 = self.canvas.create_rectangle(x1, y3, x2, y4, fill='ivory2', outline="")
+        path_5_6 = self.canvas.create_rectangle(x2, y3, x3, y4, fill='red', outline="")
+        cell_6 = self.canvas.create_rectangle(x3, y3, x4, y4, fill='ivory2', outline="")
+        path_6_7 = self.canvas.create_rectangle(x4, y3, x5, y4, fill='red', outline="")
+        cell_7 = self.canvas.create_rectangle(x5, y3, x6, y4, fill='ivory2', outline="")
+        path_7_8 = self.canvas.create_rectangle(x6, y3, x7, y4, fill='red', outline="")
+        cell_8 = self.canvas.create_rectangle(x7, y3, x8, y4, fill='ivory2', outline="")
 
         # Row 4
-        path_5_9 = self.canvas.create_rectangle(x1, y4, x2, y5, fill='red')
-        empty_4 = self.canvas.create_rectangle(x2, y4, x3, y5, fill='lightskyblue1')
-        path_6_10 = self.canvas.create_rectangle(x3, y4, x4, y5, fill='red')
-        empty_5 = self.canvas.create_rectangle(x4, y4, x5, y5, fill='lightskyblue1')
-        path_7_11 = self.canvas.create_rectangle(x5, y4, x6, y5, fill='red')
-        empty_6 = self.canvas.create_rectangle(x6, y4, x7, y5, fill='lightskyblue1')
-        path_8_12 = self.canvas.create_rectangle(x7, y4, x8, y5, fill='red')
+        path_5_9 = self.canvas.create_rectangle(x1, y4, x2, y5, fill='red', outline="")
+        empty_4 = self.canvas.create_rectangle(x2, y4, x3, y5, fill='lightskyblue1', outline="")
+        path_6_10 = self.canvas.create_rectangle(x3, y4, x4, y5, fill='red', outline="")
+        empty_5 = self.canvas.create_rectangle(x4, y4, x5, y5, fill='lightskyblue1', outline="")
+        path_7_11 = self.canvas.create_rectangle(x5, y4, x6, y5, fill='red', outline="")
+        empty_6 = self.canvas.create_rectangle(x6, y4, x7, y5, fill='lightskyblue1', outline="")
+        path_8_12 = self.canvas.create_rectangle(x7, y4, x8, y5, fill='red', outline="")
 
         # Row 5
-        cell_9 = self.canvas.create_rectangle(x1, y5, x2, y6, fill='ivory2')
-        path_9_10 = self.canvas.create_rectangle(x2, y5, x3, y6, fill='red')
-        cell_10 = self.canvas.create_rectangle(x3, y5, x4, y6, fill='ivory2')
-        path_10_11 = self.canvas.create_rectangle(x4, y5, x5, y6, fill='red')
-        cell_11 = self.canvas.create_rectangle(x5, y5, x6, y6, fill='ivory2')
-        path_11_12= self.canvas.create_rectangle(x6, y5, x7, y6, fill='red')
-        cell_12 = self.canvas.create_rectangle(x7, y5, x8, y6, fill='ivory2')
+        cell_9 = self.canvas.create_rectangle(x1, y5, x2, y6, fill='ivory2', outline="")
+        path_9_10 = self.canvas.create_rectangle(x2, y5, x3, y6, fill='red', outline="")
+        cell_10 = self.canvas.create_rectangle(x3, y5, x4, y6, fill='ivory2', outline="")
+        path_10_11 = self.canvas.create_rectangle(x4, y5, x5, y6, fill='red', outline="")
+        cell_11 = self.canvas.create_rectangle(x5, y5, x6, y6, fill='ivory2', outline="")
+        path_11_12= self.canvas.create_rectangle(x6, y5, x7, y6, fill='red', outline="")
+        cell_12 = self.canvas.create_rectangle(x7, y5, x8, y6, fill='ivory2', outline="")
 
         # # Row 6
-        path_9_13 = self.canvas.create_rectangle(x1, y6, x2, y7, fill='red')
-        empty_7 = self.canvas.create_rectangle(x2, y6, x3, y7, fill='lightskyblue1')
-        path_10_14 = self.canvas.create_rectangle(x3, y6, x4, y7, fill='red')
-        empty_8 = self.canvas.create_rectangle(x4, y6, x5, y7, fill='lightskyblue1')
-        path_11_15 = self.canvas.create_rectangle(x5, y6, x6, y7, fill='red')
-        empty_9 = self.canvas.create_rectangle(x6, y6, x7, y7, fill='lightskyblue1')
-        path_12_16 = self.canvas.create_rectangle(x7, y6, x8, y7, fill='red')
+        path_9_13 = self.canvas.create_rectangle(x1, y6, x2, y7, fill='red', outline="")
+        empty_7 = self.canvas.create_rectangle(x2, y6, x3, y7, fill='lightskyblue1', outline="")
+        path_10_14 = self.canvas.create_rectangle(x3, y6, x4, y7, fill='red', outline="")
+        empty_8 = self.canvas.create_rectangle(x4, y6, x5, y7, fill='lightskyblue1', outline="")
+        path_11_15 = self.canvas.create_rectangle(x5, y6, x6, y7, fill='red', outline="")
+        empty_9 = self.canvas.create_rectangle(x6, y6, x7, y7, fill='lightskyblue1', outline="")
+        path_12_16 = self.canvas.create_rectangle(x7, y6, x8, y7, fill='red', outline="")
 
         # Row 7
-        cell_13 = self.canvas.create_rectangle(x1, y7, x2, y8, fill='ivory2')
-        path_13_14 = self.canvas.create_rectangle(x2, y7, y8, y8, fill='red')
-        cell_14 = self.canvas.create_rectangle(x3, y7, x4, y8, fill='ivory2')
-        path_14_15 = self.canvas.create_rectangle(x4, y7, x5, y8, fill='red')
-        cell_15 = self.canvas.create_rectangle(x5, y7, x6, y8, fill='ivory2')
-        path_15_16 = self.canvas.create_rectangle(x6, y7, x7, y8, fill='red')
-        cell_16 = self.canvas.create_rectangle(x7, y7, x8, y8, fill='ivory2')
+        cell_13 = self.canvas.create_rectangle(x1, y7, x2, y8, fill='ivory2', outline="")
+        path_13_14 = self.canvas.create_rectangle(x2, y7, y8, y8, fill='red', outline="")
+        cell_14 = self.canvas.create_rectangle(x3, y7, x4, y8, fill='ivory2', outline="")
+        path_14_15 = self.canvas.create_rectangle(x4, y7, x5, y8, fill='red', outline="")
+        cell_15 = self.canvas.create_rectangle(x5, y7, x6, y8, fill='ivory2', outline="")
+        path_15_16 = self.canvas.create_rectangle(x6, y7, x7, y8, fill='red', outline="")
+        cell_16 = self.canvas.create_rectangle(x7, y7, x8, y8, fill='ivory2', outline="")
 
     def show_cell(self):
         label = Label(self.canvas, text='CURRENT LOCATION', fg='black', bg='lightskyblue1', font="bold")
@@ -194,19 +188,19 @@ class TriviaGUI(Frame):
         y4 = y3 + size
 
         # Row 1
-        nw = self.canvas.create_rectangle(x1, y1, x2, y2, fill='lightskyblue1')
-        n = self.canvas.create_rectangle(x2, y1, x3, y2, fill='red')
-        ne = self.canvas.create_rectangle(x3, y1, x4, y2, fill='lightskyblue1')
+        nw = self.canvas.create_rectangle(x1, y1, x2, y2, fill='lightskyblue1', outline="")
+        n = self.canvas.create_rectangle(x2, y1, x3, y2, fill='red', outline="")
+        ne = self.canvas.create_rectangle(x3, y1, x4, y2, fill='lightskyblue1', outline="")
 
         # Row 2
-        w = self.canvas.create_rectangle(x1, y2, x2, y3, fill='red')
-        center = self.canvas.create_rectangle(x2, y2, x3, y3, fill='ivory2')
-        e = self.canvas.create_rectangle(x3, y2, x4, y3, fill='red')
+        w = self.canvas.create_rectangle(x1, y2, x2, y3, fill='red', outline="")
+        center = self.canvas.create_rectangle(x2, y2, x3, y3, fill='ivory2', outline="")
+        e = self.canvas.create_rectangle(x3, y2, x4, y3, fill='red', outline="")
 
         # Row 3
-        sw = self.canvas.create_rectangle(x1, y3, x2, y4, fill='lightskyblue1')
-        s = self.canvas.create_rectangle(x2, y3, x3, y4, fill='red')
-        se = self.canvas.create_rectangle(x3, y3, x4, y4, fill='lightskyblue1')
+        sw = self.canvas.create_rectangle(x1, y3, x2, y4, fill='lightskyblue1', outline="")
+        s = self.canvas.create_rectangle(x2, y3, x3, y4, fill='red', outline="")
+        se = self.canvas.create_rectangle(x3, y3, x4, y4, fill='lightskyblue1', outline="")
 
 
     def show_question(self):
@@ -238,6 +232,22 @@ class TriviaGUI(Frame):
 
     def move_right(self):
         pass
+
+    # def menu_bar(self):
+    #     menu_bar = Menu(self)
+    #     file_menu = Menu(menu_bar, tearoff=0)
+    #     file_menu.add_command(label="New")
+    #     file_menu.add_command(label="Load")
+    #     file_menu.add_command(label="Save")
+    #     file_menu.add_command(label="Exit")
+    #     file_menu.add_cascade(label="File", menu=file_menu)
+    #
+    #     help_menu = Menu(self)
+    #     help_menu.add_command(label="Help")
+    #     help_menu.add_command(label="About")
+    #     help_menu.add_cascade(label="Help", menu=help_menu)
+    #
+    #     return menu_bar
 
 
 if __name__ == '__main__':
